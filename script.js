8208ac233be03b62b16a6a7f660cbe4518d53c66
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-03-05 17:47:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let fontName = document.querySelector("#fontName");
 let fontSize = document.querySelector("#fontSize");
 let writingArea = document.querySelector("#textInput");
 let linkInsert = document.querySelector("#insertLink");
+let clone = document.querySelector("#clone");
+let alignLeft = document.querySelector("#alignLeft");
+let alignJustify = document.querySelector("#alignJustify");
+let alignCenter = document.querySelector("#alignCenter");
+let alignRight = document.querySelector("#alignRight");
 
 let fontList = [
   "Arial",
@@ -24,6 +29,7 @@ const initialiser = () => {
   hightlighter(scriptButtons, true);
   hightlighter(formatButtons, false);
   hightlighter(spacingButtons, true);
+  hightlighter(clone, true);
 
   fontList.map((value) => {
     let option = document.createElement("option");
@@ -33,7 +39,7 @@ const initialiser = () => {
   });
 };
 
-for (let i = 1; i <= 25; i++) {
+for (let i = 1; i <= 7; i++) {
   let option = document.createElement("option");
   option.value = i;
   option.innerHTML = i;
@@ -48,6 +54,8 @@ const modifyText = (command, defaultUi, value) => {
 optionButton.forEach((button) => {
   button.addEventListener("click", () => {
     modifyText(button.id, false, null);
+    modifyText(fontName, false, fontName.value);
+    //   writingArea.style.font=fontName.value;
   });
 });
 
@@ -62,6 +70,30 @@ advOptionButton.forEach((button) => {
     modifyText(button.id, false, button.value);
   });
 });
+
+clone.addEventListener("click", () => {
+  // if (writingArea.select()) {
+  document.execCommand("copy");
+  // }
+});
+
+alignRight.addEventListener('click',()=>{
+    writingArea.style.textAlign="right";
+})
+
+alignLeft.addEventListener("click", () => {
+  writingArea.style.textAlign = "left";
+});
+
+alignJustify.addEventListener("click", () => {
+  writingArea.style.textAlign = "justify";
+});
+
+alignCenter.addEventListener("click", () => {
+  writingArea.style.textAlign = "center";
+});
+
+
 
 linkInsert.addEventListener("click", () => {
   let userLink = prompt("Enter a URL");
@@ -100,3 +132,6 @@ let hightlighterRemover = (className) => {
   });
 };
 window.onload = initialiser();
+
+
+
